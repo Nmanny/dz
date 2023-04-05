cb224bfbe0782951a4facde753148edfb01047ca
Nmanny <poiuytlllkiol@gmail.com>
2023-04-05 17:19:04 +0300

    1

---
+++ homework2/02-2-sdl-static/main.cxx
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include <SDL2/SDL_version.h>
+
+int main()
+{
+    using namespace std;
+    SDL_version compiled;
+    SDL_version linked;
+
+    SDL_VERSION(&compiled);
+    SDL_VERSION(&linked);
+    cout << "Compiled SDL version: " << (int)compiled.major << "."
+         << (int)compiled.minor << "." << (int)compiled.patch << endl;
+    cout << "Linked SDL version: " << (int)linked.major << "."
+         << (int)linked.minor << "." << (int)linked.patch << endl;
+    return cout.good();
+}
