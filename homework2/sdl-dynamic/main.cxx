8c19cacb9436a8c9b9eaff2eb994921e0c624732
Nmanny <poiuytlllkiol@gmail.com>
2023-04-12 13:29:16 +0300

    hw2

--- homework2/sdl-dynamic/main.cxx
+++ homework2/sdl-dynamic/main.cxx
@@ -1,10 +1,9 @@
-#include <SDL2/SDL_version.h>
+#include <SDL3/SDL_version.h>
 #include <iostream>
 
 int main()
 {
     using namespace std;
-    setenv("SDL_DYNAMIC_API", "/usr/local/lib/libSDL2-2.0.so.0.26.5", 1);
 
     SDL_version compiled;
     SDL_version linked;
